939a9780798551af5da86c2e91d85c4ccc5b4cd0
Max Walter Müller <Max@MaxBook.local>
2022-05-07 11:22:05 +0200

    forecast file_v3

--- assignment4_forecast.R
+++ assignment4_forecast.R
@@ -24,34 +24,32 @@ MSECOND <- marima(t(df), means=1, ar.pattern=structSECOND$ar.pattern,
 
 MSECOND
 
+# step slow ("step.slow.p.marima_2017.R" function in the same working directory is necessary!)
 slMSECOND <- step.slow.p(MSECOND, data=t(df))
 slMSECOND
 
 
-## Forecast ######
-
-####### THIS PART NEEDS TO BE REWRITTEN ##########
+##################### Forecast #############################
 
 predictiondf <- df[1:1077,] 
-actualdf <- df
-
-#add new rows 
+#add new rows for predicted/forecasted values
 newData <- matrix(0, nrow = 4, ncol = 10)
 newData[,1] <- seq(201609,201612,1) #add dates we want to predict for
 newData[,2:10] <- 0 
 colnames(newData) <- colnames(predictiondf)
+
 #add prediction data to main df
 MatPredicts <- rbind(predictiondf, newData)
-#do forecast
+#do forecasts
 Forecasts <-  arma.forecast(t(MatPredicts), nstart=1077, nstep=4, marima=slMSECOND) # named smething different####
-
+# move forecasted values to dataframe
 forecastdf <- data.frame(
-  t(Forecasts$forecasts),
-  
+  t(Forecasts$forecasts)
 )
+# adjust dataframe with right values
 forecastdf[,1] <- df[,1]
 colnames(forecastdf) <- colnames(predictiondf)
-forecastdf$date <- lubridate::ym(forecastdf$yyyymm)
+forecastdf$date <- lubridate::ym(forecastdf$yyyymm) # attention: moves date column to last column of the table
 forecastdf <- forecastdf[, 2:11]
 forecastdf[1:1076,1:9] <- NA 
 forecastdf[1077,1:9] <- plottingDf[1077,1:9]
@@ -68,20 +66,21 @@ colnames(emptyMatrix) <- colnames(df)
 df <- rbind(emptyMatrix,c(plottingDf[1077,i-1], plottingDf[1077,i-1]), df)
 return(df)
 }
-
-
-plottingDfReduced <- plottingDf[1031:1081,]
-plottingDfReduced <- forecastDf[1031:1081,]
-
 # set colors for graph
 colors <- c("#F8766D", "#7CAE00", "#00BFC4", "#C77CFF", "#C59900","black","grey70")
 
-otherInterval <- intervalFunc(6, plottingDf)
-presenting <- ggplot() + 
-  geom_line(data=forecastdf[1070:1081,], aes(x=date, y=Other, color = 'Forecast')) + 
-  geom_line(data=plottingDf[1070:1081,], aes(x=date, y=Other, color = 'Actual Data')) +
+
+######################### Forecast PLOTS ################################
+
+######### PLOT Cnsmr #################
+# Extracts the right intervals for the different columns to plot (e.g.: Column "2" -> "Cnsmr" to "6" -> "Other")
+CnsmrInterval <- intervalFunc(2, plottingDf)
+# prepares the plot function
+presentingCnsmr <- ggplot() + 
+  geom_line(data=forecastdf[1070:1081,], aes(x=date, y=Cnsmr, color = 'Forecast')) + 
+  geom_line(data=plottingDf[1070:1081,], aes(x=date, y=Cnsmr, color = 'Actual Data')) +
   geom_vline( aes(xintercept = plottingDf$date[1077], color = 'Last Observation'),linetype = "dashed") +
-  geom_ribbon(data = otherInterval, 
+  geom_ribbon(data = CnsmrInterval, 
               aes(x = forecastdf[1071:1081,'date'],
                   ymin = lower.lim, ymax = upper.lim, color = '95% Prediction Interval'), 
               alpha = 0.1,
@@ -91,31 +90,93 @@ presenting <- ggplot() +
   scale_color_manual(values = c(colors[7], colors[1], colors[2], colors[6]))+
   labs(colour="")+
   theme(legend.position="bottom")
-presenting
+# plots the forecast plot
+presentingCnsmr
 
-OtherPlot <- ggplot() + geom_line(data=Forecasts$forecasts, aes(x=date, y=Other, color = colors[1]))
-ggplotly(OtherPlot)
+######### PLOT Manuf #################
+# Extracts the right intervals for the different columns to plot (e.g.: Column "2" -> "Cnsmr" to "6" -> "Other")
+ManufInterval <- intervalFunc(3, plottingDf)
+# prepares the plot function
+presentingManuf <- ggplot() + 
+  geom_line(data=forecastdf[1070:1081,], aes(x=date, y=Manuf, color = 'Forecast')) + 
+  geom_line(data=plottingDf[1070:1081,], aes(x=date, y=Manuf, color = 'Actual Data')) +
+  geom_vline( aes(xintercept = plottingDf$date[1077], color = 'Last Observation'),linetype = "dashed") +
+  geom_ribbon(data = ManufInterval, 
+              aes(x = forecastdf[1071:1081,'date'],
+                  ymin = lower.lim, ymax = upper.lim, color = '95% Prediction Interval'), 
+              alpha = 0.1,
+              fill = "grey70")+
+  theme_light()+
+  scale_linetype_manual(values=c("solid","solid","dashed","solid"))+
+  scale_color_manual(values = c(colors[7], colors[1], colors[2], colors[6]))+
+  labs(colour="")+
+  theme(legend.position="bottom")
+# plots the forecast plot
+presentingManuf
 
-plot(Forecasts$forecasts)
-#Extract predictions and upper and lower limits
-Year<-t(df2MatPredicts[103:112,1]);
-Predict<-Forecasts$forecasts[3,103:112]
-stdv<-sqrt(Forecasts$pred.var[3,3,2:11])
-upper.lim=Predict+stdv*1.96
-lower.lim=Predict-stdv*1.96
-Out<-rbind(Year,Predict,upper.lim,lower.lim)
-print(Out)
+######### PLOT HiTec #################
+# Extracts the right intervals for the different columns to plot (e.g.: Column "2" -> "Cnsmr" to "6" -> "Other")
+HiTecInterval <- intervalFunc(4, plottingDf)
+# prepares the plot function
+presentingHiTec <- ggplot() + 
+  geom_line(data=forecastdf[1070:1081,], aes(x=date, y=HiTec, color = 'Forecast')) + 
+  geom_line(data=plottingDf[1070:1081,], aes(x=date, y=HiTec, color = 'Actual Data')) +
+  geom_vline( aes(xintercept = plottingDf$date[1077], color = 'Last Observation'),linetype = "dashed") +
+  geom_ribbon(data = HiTecInterval, 
+              aes(x = forecastdf[1071:1081,'date'],
+                  ymin = lower.lim, ymax = upper.lim, color = '95% Prediction Interval'), 
+              alpha = 0.1,
+              fill = "grey70")+
+  theme_light()+
+  scale_linetype_manual(values=c("solid","solid","dashed","solid"))+
+  scale_color_manual(values = c(colors[7], colors[1], colors[2], colors[6]))+
+  labs(colour="")+
+  theme(legend.position="bottom")
+# plots the forecast plot
+presentingHiTec
 
-# plot results:
-plot(df2MatPredicts[1:112,1], Forecasts$forecasts[3,],type="l", xlab="Year",
-     ylab="Change in Log Number of Words", main="Prediction of change in log number of words")
-lines(df2MatPredicts[1:102,1], df2MatPredicts[1:102,3], type="p")
-grid(lty=3, lwd=1, col="grey")
-Years<-seq(2021,2030,1)
-lines(Years, Predict, type="l", col=2)
-lines(Years, upper.lim, type="l", col=2)
-lines(Years, lower.lim, type="l", col=2)
-lines(c(2021,2020), c(0,2), col=4)
+######### PLOT Hlth #################
+# Extracts the right intervals for the different columns to plot (e.g.: Column "2" -> "Cnsmr" to "6" -> "Other")
+HlthInterval <- intervalFunc(5, plottingDf)
+# prepares the plot function
+presentingHlth <- ggplot() + 
+  geom_line(data=forecastdf[1070:1081,], aes(x=date, y=Hlth, color = 'Forecast')) + 
+  geom_line(data=plottingDf[1070:1081,], aes(x=date, y=Hlth, color = 'Actual Data')) +
+  geom_vline( aes(xintercept = plottingDf$date[1077], color = 'Last Observation'),linetype = "dashed") +
+  geom_ribbon(data = HlthInterval, 
+              aes(x = forecastdf[1071:1081,'date'],
+                  ymin = lower.lim, ymax = upper.lim, color = '95% Prediction Interval'), 
+              alpha = 0.1,
+              fill = "grey70")+
+  theme_light()+
+  scale_linetype_manual(values=c("solid","solid","dashed","solid"))+
+  scale_color_manual(values = c(colors[7], colors[1], colors[2], colors[6]))+
+  labs(colour="")+
+  theme(legend.position="bottom")
+# plots the forecast plot
+presentingHlth
+
+######### PLOT Other #################
+# Extracts the right intervals for the different columns to plot (e.g.: Column "2" -> "Cnsmr" to "6" -> "Other")
+OtherInterval <- intervalFunc(6, plottingDf)
+# prepares the plot function
+presentingOther <- ggplot() + 
+  geom_line(data=forecastdf[1070:1081,], aes(x=date, y=Other, color = 'Forecast')) + 
+  geom_line(data=plottingDf[1070:1081,], aes(x=date, y=Other, color = 'Actual Data')) +
+  geom_vline( aes(xintercept = plottingDf$date[1077], color = 'Last Observation'),linetype = "dashed") +
+  geom_ribbon(data = OtherInterval, 
+              aes(x = forecastdf[1071:1081,'date'],
+                  ymin = lower.lim, ymax = upper.lim, color = '95% Prediction Interval'), 
+              alpha = 0.1,
+              fill = "grey70")+
+  theme_light()+
+  scale_linetype_manual(values=c("solid","solid","dashed","solid"))+
+  scale_color_manual(values = c(colors[7], colors[1], colors[2], colors[6]))+
+  labs(colour="")+
+  theme(legend.position="bottom")
+# plots the forecast plot
+presentingOther
+
 
 
 
